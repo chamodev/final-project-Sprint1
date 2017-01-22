a78881b604dd8cf2272418ea2d87cc99c65dc758
Rodulfo <rodulfo.prieto@ayuinc.com>
2017-01-22 16:45:19 -0600

    Initial commit

---
+++ resources.js
@@ -0,0 +1,19 @@
+
+
+
+/*
+NOTAS CHAMO: crear 4 nuevos estudiantes
+
+var student = new Student ('Ana Lucia Porras', 80, 90);
+students.push(student);
+var student = new Student ('Susana Brito', 65, 95);
+students.push(student);
+var student = new Student ('Milagros Ortiz', 90, 40);
+students.push(student);
+var student = new Student ('Luciana Quiroz', 35, 70);
+students.push(student);
+var student = new Student ('Valerie Martinez', 35, 30);
+students.push(student);
+printHTML(mergeHTML());
+
+*/
